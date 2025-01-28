326093404b39de0aa9e0eab4598af1f5fe8f83fc
kavinduni <118064492+kavinduni@users.noreply.github.com>
2025-01-28 08:05:52 +0530

    Update config-v2.js

--- config-v2.js
+++ config-v2.js
@@ -1,6 +1,6 @@
 const GITHUB_AUTH_TOKEN = "null"
 const BOT_NUMBER = "null"
-const SESSION_ID = "PRABATH-MD~xfkwRKyS#6MftoCeH4h_Cna7mThGCJEgO4vfvIprBT07gQN5iWus"
+const SESSION_ID = ""
 
 
 module.exports = {
